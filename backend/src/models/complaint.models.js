dfa8578586f4b373a5685adfdef773f572bc6146
Ayush Sinha <as.ayushh1@gmail.com>
2026-07-24 15:33:46 +0530

    models finished

--- backend/src/models/complaint.models.js
+++ backend/src/models/complaint.models.js
@@ -9,6 +9,67 @@ const complaintSchema = new Schema(
         title: {
             type: String,
             required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+            enum: ["electricity", "water", "food", "miscellaneous"],
+            required: true
+        },
+        imageProof: {
+            type: String,
+            required: true
+        },
+        isAnonymous: {
+            type: Boolean,
+            default: false
+        },
+        filedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        assignedTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        status: {
+            type: String,
+            enum: ["pending", "in-progress", "resolved"],
+            default: "pending"
+        },
+        priority: {
+            type: String,
+            enum: ["normal", "high"],
+            default: "normal"
+        },
+        upvotes:[
+             {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
         }
-    }
+        ],
+        escalated: {
+            type: Boolean,
+            default: false
+        },
+        isInvalid: {
+            type: Boolean,
+            default: false
+        },
+        filedAt: {
+            type: Date
+        },
+        lastUpdatedAt: {
+            type: Date,
+        },
+        resolvedAt: {
+            type: Date
+        },
+    },
+    {timestamps: true}
 )
+
+export const Complaint = mongoose.model("Complaint", complaintSchema)
