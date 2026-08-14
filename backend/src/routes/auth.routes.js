d38eecaec0ef14c53c1883cd7b7ba7aa45f59b4a
Ayush Sinha <as.ayushh1@gmail.com>
2026-08-14 15:10:54 +0530

    feat: frontend created

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,14 +1,14 @@
 import {Router} from "express"
 import {registerUser, loginUser} from "../controllers/auth.controllers.js"
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js"
-import { validate } from "../validators/validator.middleware.js"
+
 
 const router = Router()
 
 
 
-router.post("/register", userRegisterValidator(), validate, registerUser)
+router.post("/register", userRegisterValidator(),  registerUser)
 
-router.post("/login", userLoginValidator(), validate, loginUser)
+router.post("/login", userLoginValidator(), loginUser)
 
 export default router
---
+++ frontend/civc-pulse/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login(){
+    return (
+        <div>
+            <h1 className="">Login Page for civic pulse</h1>
+        </div>
+    )
+}
+
+export default Login
