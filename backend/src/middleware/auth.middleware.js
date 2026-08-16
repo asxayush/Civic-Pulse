e5eaefca96451275b0ad3e242a823248d4167352
Ayush Sinha <as.ayushh1@gmail.com>
2026-08-16 18:52:53 +0530

    feat: implement verifyJWT authentication middleware to validate user access tokens

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,7 +2,32 @@ import jwt from "jsonwebtoken"
 import { asyncHandler } from "../utils/asyncHandlers.js"
 import { ApiError } from "../utils/api-error.js"
 import { ApiResponse } from "../utils/api-response.js"
+import {User} from "mongoose"
+
+// jwt decode code to be written 
+
+export const verifyJWT = asyncHandler (async (req, res, next) => {
+    const token = req.header("Authorization")?.replace("Bearer ","")
+
+    if(!token) {
+        throw new ApiError (401, "Unauthorized access")
+    }
+
+        try {
+       const decodedToken  =  jwt.verify(token, process.env.JWT_SECRET)
+       console.log("DECODED:", decodedToken)
+      const user =  await User.findById(decodedToken?._id).select( "-password")
+      console.log("USER:", user)
 
 
-// jwt decode code to be written \
-
+      if(!user){
+        console.log("JWT ERROR:", error.message) 
+        throw new ApiError(401, "invalid access token")
+      }
+      req.user = user
+      next()
+    } catch (error) {
+        throw new ApiError(401, "invalid access token")
+    }
+}
+)
