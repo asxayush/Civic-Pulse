bf468805b0070d7c42c3548e14ef1d1c5daeb771
Ayush Sinha <as.ayushh1@gmail.com>
2026-08-19 22:49:52 +0530

    feat: implement request validation middleware and initialize project authentication routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,16 +2,17 @@ import {Router} from "express"
 import {registerUser, loginUser, getCurrentUser} from "../controllers/auth.controllers.js"
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js"
 import { verifyJWT } from "../middleware/auth.middleware.js"
+import { validate } from "../middleware/validators.middleware.js"
 
 
 const router = Router()
 
 
 
-router.route("/register").post( userRegisterValidator(),  registerUser)
+router.route("/register").post( userRegisterValidator(), validate,  registerUser)
 
-router.route("/login").post(userLoginValidator(), loginUser)
+router.route("/login").post(userLoginValidator(),validate, loginUser)
 
-router.route('/getMe').get(verifyJWT, getCurrentUser)
+router.route('/getMe').get(verifyJWT, validate, getCurrentUser)
 
 export default router
