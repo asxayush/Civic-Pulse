a4604cd902c5260f3349a03d754fcaeea47f48de
Ayush Sinha <as.ayushh1@gmail.com>
2026-08-22 00:41:54 +0530

    feat: implement script to seed database with demo user accounts

--- backend/src/ensureDemoUsers.js
+++ backend/src/ensureDemoUsers.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
+import dns from "dns";
 import mongoose from "mongoose";
 import ConnectDB from "./db/schema.js";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 import { User } from "./models/user.models.js";
 
 const accounts = [
